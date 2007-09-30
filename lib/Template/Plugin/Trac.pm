22619aa210c0658b81b612f512c085dd1ee608c0
Graham TerMarsch <graham@howlingfrog.com>
2007-09-30 22:43:00 +0000

    Added POD documentation for methods.

--- lib/Template/Plugin/Trac.pm
+++ lib/Template/Plugin/Trac.pm
@@ -1,12 +1,23 @@
 package Template::Plugin::Trac;
 
+###############################################################################
+# Required inclusions.
+###############################################################################
 use strict;
 use warnings;
 use Text::Trac;
 use base qw(Template::Plugin::Filter);
 
+###############################################################################
+# Version number.
+###############################################################################
 our $VERSION = '1.01_01';
 
+###############################################################################
+# Subroutine:   init()
+###############################################################################
+# Initializes the template plugin.
+###############################################################################
 sub init {
     my $self = shift;
     $self->{'_DYNAMIC'} = 1;
@@ -14,6 +25,11 @@ sub init {
     return $self;
 }
 
+###############################################################################
+# Subroutine:   filter($txt, $args, $config)
+###############################################################################
+# Filters the given text, by running it through 'Text::Trac'.
+###############################################################################
 sub filter {
     my ($self, $text, $args, $config) = @_;
     my $parser = Text::Trac->new( %{$config} );
@@ -42,13 +58,27 @@ Template::Plugin::Trac - TT filter for Text::Trac
 C<Template::Plugin::Trac> is a plugin for TT, which formats your text using
 Trac's WikiFormatting style.
 
+=head1 METHODS
+
+=over
+
+=item init()
+
+Initializes the template plugin. 
+
+=item filter($txt, $args, $config)
+
+Filters the given text, by running it through C<Text::Trac>. 
+
+=back
+
 =head1 AUTHOR
 
 Graham TerMarsch <cpan@howlingfrog.com>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2006, Graham TerMarsch.  All rights reserved.
+Copyright (C) 2006-2007, Graham TerMarsch.  All rights Reserved.
 
 This is free software; you can redistribute it and/or modify it under the same
 terms as Perl itself.
